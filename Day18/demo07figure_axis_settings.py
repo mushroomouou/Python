1c13d27acf3431831daa9e356e04b7715823bb1c
Mushroom <72346929+mushroomouou@users.noreply.github.com>
2021-03-14 22:59:31 +0800

    Delete Day18 directory

--- Day18/demo07figure_axis_settings.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x_values = np.arange(-5,5,0.1)
-y1 = [x**2 for x in x_values]
-y2 = [x + 2 for x in x_values]
-plt.plot(x_values,y1)
-plt.plot(x_values,y2,linestyle="--")
-plt.show()
